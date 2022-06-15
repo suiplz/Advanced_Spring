8f0b4b787e1ab073b3139a8136d7793c996c8aca
suiplz <acepade231@gmail.com>
2022-06-15 14:28:54 +0900

    [Initial commit] 22-06-15

---
+++ src/main/java/hello/advanced/aop/OrderRepositoryV0.java
@@ -0,0 +1,28 @@
+package hello.advanced.aop;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        //저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생");
+        }
+
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
