72c30c8e78e4b55eaf776bdf9165e97509291f41
suiplz <acepade231@gmail.com>
2022-06-18 17:25:23 +0900

    [Proxy] 22-06-18

--- src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping
 @ResponseBody
 public interface OrderControllerV1 {
-
     @GetMapping("/v1/request")
     String request(@RequestParam("itemId") String itemId);
 
